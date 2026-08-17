0218071322f79514a1462f320a5c91f5972d175c
vyakhya015-dev <vyakhya015@gmail.com>
2026-08-18 00:17:56 +0530

    Add files via upload

---
+++ vyakhya.c
@@ -0,0 +1,19 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+int main()
+{
+    int num1, num2, sum;
+
+    printf("Enter first number:  ");
+    scanf("%d" , &num1);
+    
+    printf("Enter second number:  ");
+    scanf("%d" , &num2);
+
+    sum = num1 + num2;
+    printf("Sum = %d\n", sum);
+
+    return 0;
+
+}
