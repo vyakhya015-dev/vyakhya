e3afb67cec16a094cb897ae178889c2643fbba1a
vyakhya015-dev <vyakhya015@gmail.com>
2026-08-25 10:18:41 +0530

    Add files via upload

---
+++ 4vyakhya.c
@@ -0,0 +1,13 @@
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+  int main()
+  {
+    int Celsius = 25, Fahrenheit;
+
+   Fahrenheit = (Celsius * 9.0 / 5.0) + 32;
+   printf("Fahrenheit = %d\n", Fahrenheit);
+
+   return 0;
+
+  } 
