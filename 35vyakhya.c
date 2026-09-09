f64d09302b955ea6778346a8a12268eb7dad2ffa
vyakhya015-dev <vyakhya015@gmail.com>
2026-09-09 12:48:55 +0530

    Add files via upload

---
+++ 35vyakhya.c
@@ -0,0 +1,22 @@
+// Q36: Write a program to find the HCF (GCD) of two numbers.
+
+#include  <stdio.h>
+int main()
+{
+    int a, b, i, hcf;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    for(i = 1; i <= a && i <= b; i++)
+    {
+        if(a % i == 0 && b % i== 0)
+        {
+            hcf = i;
+        }
+    }
+    printf("hcf = %d", hcf);
+
+    return 0;
+
+}
