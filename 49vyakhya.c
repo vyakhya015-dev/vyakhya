fb03799af4fd38d9f34cfefd2b3a3a7ac36a598c
vyakhya015-dev <vyakhya015@gmail.com>
2026-09-14 11:21:16 +0530

    Add files via upload

---
+++ 49vyakhya.c
@@ -0,0 +1,26 @@
+// Q50: Write a program to print the following pattern:
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 5; i >= 1; i--)
+    {
+        for(j = 1; j <= 5-i; j++)
+        {
+            printf(" ");
+        }
+
+        for(j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+
+    }
+    return 0;
+
+}
